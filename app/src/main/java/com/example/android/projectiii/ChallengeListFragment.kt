7110ac23445ea5e12c82251c127a4a0aee135fa2
Maxime Magné <mmagne@easilys.com>
2019-10-24 12:44:24 +0200

    Refactor to fragment and correct nav bar

--- app/src/main/java/com/example/android/projectiii/ChallengeListFragment.kt
+++ app/src/main/java/com/example/android/projectiii/ChallengeListFragment.kt
@@ -9,19 +9,18 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-import com.example.android.projectiii.databinding.ActivityMainBinding
+import com.example.android.projectiii.databinding.ChallengeListBinding
 import com.example.android.projectiii.mainView.MyRecyclerViewAdapter
 
-class ChallengeList : Fragment() {
+class ChallengeListFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        super.onCreate(savedInstanceState)
-
-        val binding: ActivityMainBinding =
+        super.onCreateView(inflater, container, savedInstanceState)
+        val binding: ChallengeListBinding =
             DataBindingUtil.inflate(inflater, R.layout.challenge_list, container, false)
 
         val list = ArrayList<Levels>()
@@ -55,6 +54,6 @@ class ChallengeList : Fragment() {
         binding.currentChallengesList.layoutManager = LinearLayoutManager(activity, RecyclerView.VERTICAL, false)
         binding.currentChallengesList.adapter = adapter
 
-        return super.onCreateView(inflater, container, savedInstanceState)
+        return binding.root
     }
 }
--- app/src/main/java/com/example/android/projectiii/MainActivity.kt
+++ app/src/main/java/com/example/android/projectiii/MainActivity.kt
@@ -2,32 +2,50 @@ package com.example.android.projectiii
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.FragmentTransaction
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var challengeList: ChallengeList
+    lateinit var challengeList: ChallengeListFragment
+    lateinit var expertList: ExpertListFragment
+    lateinit var completedChallenges: CompletedChallengeFragment
+    private val fragmentManager: FragmentManager = supportFragmentManager
+    lateinit var active: Fragment
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val bottomNavigation : BottomNavigationView = findViewById(R.id.bottom_nav)
+        challengeList = ChallengeListFragment()
+        expertList = ExpertListFragment()
+        completedChallenges = CompletedChallengeFragment()
+
+        fragmentManager.beginTransaction().add(R.id.fragment_container, expertList, "3").hide(expertList).commit()
+        fragmentManager.beginTransaction().add(R.id.fragment_container, challengeList, "2").hide(challengeList).commit()
+        fragmentManager.beginTransaction().add(R.id.fragment_container, completedChallenges, "1").commit()
+
+        active = completedChallenges
 
         bottomNavigation.setOnNavigationItemSelectedListener { item ->
 
             when (item.itemId){
-
-                R.id.all -> {
-                    challengeList = ChallengeList()
-                    supportFragmentManager
-                        .beginTransaction()
-                        .replace(R.id.fragment_container, challengeList)
-                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-                        .commit()
+                R.id.completed -> {
+                    fragmentManager.beginTransaction().hide(active).show(completedChallenges).commit()
+                    active = completedChallenges
                 }
 
+                R.id.all -> {
+                    fragmentManager.beginTransaction().hide(active).show(challengeList).commit()
+                    active = challengeList
+                }
+
+                R.id.expert -> {
+                    fragmentManager.beginTransaction().hide(active).show(expertList).commit()
+                    active = expertList
+                }
             }
             true
         }
