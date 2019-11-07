c8f0a50a4644daa6ea4dc51d6c362800b7b42b27
unknown <lukasgrin8@gmail.com>
2019-11-07 14:59:49 +0100

    coins

--- app/src/main/java/com/example/android/projectiii/Employee/Employee.kt
+++ app/src/main/java/com/example/android/projectiii/Employee/Employee.kt
@@ -1,4 +1,10 @@
 package com.example.android.projectiii.Employee
 
-data class Employee (val name: String, val email: String, val password: String)
+import android.util.Log
 
+class Employee (val name: String, val email: String, val password: String, var totalCoins: Int){
+    fun addCoins(nrOfCoins: Int){
+        totalCoins += nrOfCoins
+        Log.d("Check", totalCoins.toString())
+    }
+}
